39c6552f0a9a924469b6bca48a1372e275354954
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-06-28 12:33:33 +0530

    basic workings are almost done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,52 @@
+import { useEffect, useState } from "react";
 import Editor from "./Editor";
 
 function App() {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSrcDoc(`
+      <html>
+      <body>
+      ${html}
+      </body>
+      <style>${css}</style>
+      <script>${js}</script>
+      </html>
+  `)
+    }, 250);
+  })
+
 
   return (
     <>
       <div className="pane top-pane">
-        <Editor />
-        <Editor />
-        <Editor/> 
+        <Editor
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+          language="xml"
+        />
+        <Editor
+          displayName="Css"
+          value={css}
+          onChange={setCss}
+          language="css"
+        />
+        <Editor
+          displayName="JavaScript"
+          value={js}
+          onChange={setJs}
+          language="javascript"
+        />
       </div>
       <div className="pane">
         <iframe
+          srcDoc={srcDoc}
           title="output"
           sandbox="allow-script"
           width="100%"
--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
@@ -8,30 +9,40 @@ import { Controlled as ControlledEditor } from "react-codemirror2";
 
 const Editor = (props) => {
     const { displayName, language, value, onChange } = props;
-    
-    const handleChange = (data,value,editor) => {
-        onChange(value)
-    }
-  return (
-    <div className="editor-container">
-      <div className="editor-title">
-        {displayName}
-        <button>o/c</button>
-      </div>
-      <ControlledEditor
-        onBeforeChange={handleChange}
-        value={value}
-        className="code-mirror-wrapper"
-        options={{
-          lineWrapping: true,
-          lint: true,
-            mode: language,
-          theme:'material',
-          lineNumbers: true,
-        }}
-      />
-    </div>
-  );
+
+
+    const handleChange = (editor, data, value) => {
+        onChange(value);
+    };
+
+    return (
+        <div className="editor-container">
+            <div className="editor-title">
+                {displayName}
+                <button>o/c</button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    theme: 'material',
+                    lineNumbers: true,
+                }}
+            />
+        </div>
+    );
+};
+
+Editor.propTypes = {
+    displayName: PropTypes.string.isRequired,
+    language: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
 };
 
 export default Editor;
+
