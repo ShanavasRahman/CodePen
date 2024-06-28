779f44239c88e5481e2953f99bcac6252c6254ea
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-06-28 11:24:12 +0530

    editor component created

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-script"
+          width="100%"
+          height="100%"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </>
+  );
+}
+
+export default App;
